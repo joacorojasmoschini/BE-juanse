f253ba8c467541251ca81dff3ed0ee1c31627204
Joaquin <joaco.vla@gmail.com>
2023-10-20 17:30:36 -0300

    some fixes

--- src/routes/catalogo.js
+++ src/routes/catalogo.js
@@ -14,8 +14,8 @@ const router = Router();
 // Agregar checksession
 router.get("/:website", getAllCtrl);
 router.get("/:website/:id", getCtrl);
-router.post("/:website", upload.single("image") , createCtrl);
-router.put("/:website/:id", checkSession, updateCtrl);
+router.post("/:website", upload.single("image"), createCtrl);
+router.put("/:website/:id", upload.single("image"), updateCtrl);
 router.delete("/:website/:id", deleteCtrl);
 
 module.exports = { router };
