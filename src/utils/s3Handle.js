1bde05e1a262258c5a34e5d5a85cb5cec8cfd98e
Fede Terpin <federicoterpin@gmail.com>
2023-11-30 10:27:19 -0300

    added bucket name

--- src/utils/s3Handle.js
+++ src/utils/s3Handle.js
@@ -3,6 +3,7 @@ const { S3Client } = require('@aws-sdk/client-s3');
 
 const config = {
     region: process.env.AWS_REGION,
+    bucket: process.env.AWS_BUCKET_NAME,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
