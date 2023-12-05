a1eac4cb3e711d3073b19440cb385b544cb8ad54
Fede Terpin <federicoterpin@gmail.com>
2023-12-05 18:44:03 -0300

    https config

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const fs = require('fs');
+const https = require('https');
 const { router } = require("./routes");
 const db = require("./config/db");
 const cookieParser = require("cookie-parser");
 
+// Crear la aplicación Express
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true
-}));
+// Middleware y rutas
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(router);
 app.use(cookieParser());
+
+// Conectar a la base de datos
 db().then(() => console.log("db connected"));
+
+// Opciones para HTTPS
+const options = {
+  key: fs.readFileSync('/etc/ssl/mysite.key'),
+  cert: fs.readFileSync('/etc/ssl/mysite.crt')
+};
+
+https.createServer(options, app).listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
 module.exports = app;
